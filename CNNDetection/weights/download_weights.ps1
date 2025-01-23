0cbf0a645094964b69dc7e47047ece52db4b2cda
Martin Hansen <martin8924@proton.me>
2025-01-23 23:03:29 +0100

    Attempt to fix powershell file

--- CNNDetection/weights/download_weights.ps1
+++ CNNDetection/weights/download_weights.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -URI https://www.dropbox.com/s/2g2jagq2jn1fd0i/blur_jpg_prob0.5.pth?dl=0 -OutFile CNNDetection/weights/blur_jpg_prob0.1.pth
+Invoke-WebRequest -URI https://www.dropbox.com/s/2g2jagq2jn1fd0i/blur_jpg_prob0.5.pth?dl=0 -OutFile CNNDetection/weights/blur_jpg_prob0.5.pth

2c9d35f55b5bef5f0b5e57aaac12547fc23c35a1
Martin Hansen <martin8924@proton.me>
2025-01-23 12:13:46 +0100

    Comment out incomplete test

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -1,13 +1,13 @@
-import requests
+#import requests
 
-url = "http://127.0.0.1:8000/report"
-n = 10
-response = requests.post(url, json={"n":n})
+#url = "http://127.0.0.1:8000/report"
+#n = 10
+#response = requests.post(url, json={"n":n})
 # response = requests.post(url, json={"n": n})
 
-if response.status_code == 200:
-    # Save the HTML content from the response
-    with open("report.html", "w", encoding="utf-8") as f:
-        f.write(response.text)
-else:
-    print(f"Failed to generate report. Status code: {response.status_code}")
+#if response.status_code == 200:
+#    # Save the HTML content from the response
+#    with open("report.html", "w", encoding="utf-8") as f:
+#        f.write(response.text)
+#else:
+#    print(f"Failed to generate report. Status code: {response.status_code}")
